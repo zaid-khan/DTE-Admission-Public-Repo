1ac1a476ea3b67fc7ba16b5aec22fae1f3a77f63
vyankatesh <vyankateshberde24@gmail.com>
2018-10-11 22:41:40 +0530

    Worked on FAQ

--- app/src/main/java/com/example/mcaadmissionhelp/MainActivity.java
+++ app/src/main/java/com/example/mcaadmissionhelp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mcaadmissionhelp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.faq)
+    void onFaqClick() {
+        Intent intent = new Intent(this, FAQ.class);
+        startActivity(intent);
+    }
+
+
 }
